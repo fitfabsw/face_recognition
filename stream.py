a5e0f4a65495bd0629ea7050047bd9977bc9a766
simon <ficstudio@gmail.com>
2024-11-20 08:36:30 +0800

    Update stream.py
    Move some stream.py code to who_lib.py.

--- stream.py
+++ stream.py
@@ -4,42 +4,10 @@ import numpy as np
 import face_recognition
 import os
 import pickle
+from who_lib import save_encodings_to_file, load_encodings_from_file, get_knowns_encodings_multi
 
 knowns_dir = "known"
 knowns = os.listdir(knowns_dir)
-
-
-def save_encodings_to_file(encodings, names, filename="encodings.pkl"):
-    with open(filename, "wb") as f:
-        pickle.dump((encodings, names), f)
-
-
-def load_encodings_from_file(filename="encodings.pkl"):
-    if os.path.exists(filename):
-        with open(filename, "rb") as f:
-            return pickle.load(f)
-    return None, None
-
-
-def get_knowns_encodings_multi():
-    known_encodings = []
-    known_names = []
-    # 假設每個人有一個文件夾，文件夾名稱即人名
-    base_dir = "known-multiple"
-    for person_name in os.listdir(base_dir):
-        person_dir = os.path.join(base_dir, person_name)
-        # 確保是目錄
-        if os.path.isdir(person_dir):
-            for filename in os.listdir(person_dir):
-                image_path = os.path.join(person_dir, filename)
-                image = face_recognition.load_image_file(image_path)
-                encodings = face_recognition.face_encodings(image)
-                if encodings:
-                    known_encodings.append(encodings[0])  # 每張圖片的第一個人臉編碼
-                    known_names.append(person_name)  # 使用文件夾名稱作為人名
-                    print(f"Loaded encoding for {person_name} from {filename}")
-    return known_encodings, known_names
-
 
 def main():
     # known_encodings = load_encodings_from_file()
@@ -47,7 +15,7 @@ def main():
     if os.path.exists("encodings.pkl"):
         known_encodings, known_names = load_encodings_from_file()
     else:
-        known_encodings, known_names = get_knowns_encodings_multi()
+        known_encodings, known_names = get_knowns_encodings_multi("known-multiple")
         save_encodings_to_file(known_encodings, known_names, filename="encodings.pkl")
     t1 = time.time()
     print(f"Time to load encodings: {t1 - t0} seconds")
